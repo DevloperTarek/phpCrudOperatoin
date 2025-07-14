88770d572960877493e3457124f910f000ddc438
Tarek Hossain <devlopertarek433@gmail.com>
2025-07-14 16:57:51 +0600

    php substraction

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 //    Multiplication
 $multi = 10;
 $res = $multi *=10;
-echo "the Muliplication Result is" . $res;
+echo "the Muliplication Result is" . $res . "<br>";
+// Substraction
+$subs = 10;
+$suRes = $subs -= 10;
+echo "The Substraction Result is" . $suRes;
 ?>
